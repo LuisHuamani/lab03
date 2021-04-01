13c0ba960cd127b5a9b51c726cfab159a63e764f
Luis Huamani <luis.huamani.c@tecsup.edu.pe>
2021-03-31 23:05:45 -0500

    Tercero

--- src/Statistics.js
+++ src/Statistics.js
@@ -1,12 +1,18 @@
 const Statistics = (props) => {
+    const {good,neutral, bad} = props
     return(
         <div>
-            <p>Good:{props.good}</p>
-            <p>Neutral:{props.neutral}</p>
-            <p>Bad:{props.bad}</p>
-            <p>All:{props.all}</p>
-            <p>Average:{props.average}</p>
-            <p>Positive:{props.positive}</p>
+            {good || neutral || bad?
+            <div>
+                <p>Good:{props.good}</p>
+                <p>Neutral:{props.neutral}</p>
+                <p>Bad:{props.bad}</p>
+                <p>All:{props.all}</p>
+                <p>Average:{props.average}</p>
+                <p>Positive:{props.positive}</p>
+            </div>
+            :
+            <h3>No feedback given</h3>}
         </div>
     )
 }

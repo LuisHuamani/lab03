c7c85c2ea7cd3442ca32c54859c3e6285940727a
Luis Huamani <luis.huamani.c@tecsup.edu.pe>
2021-03-31 21:34:57 -0500

    Primero

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const clicksgood = () => {
+    setGood(good+1)
+    setAll(all+1)
+    setAverage(average+0.1)
+    setPositive(((good+1)*100)/(all+1))
+  }
+
+  const clicksneutral = () => {
+    setNeutral(neutral+1)
+    setAll(all+1)
+    setAverage(average+0)
+    setPositive(((good)*100)/(all+1))
+  }
+
+  const clicksbad = () => {
+    setBad(bad+1)
+    setAll(all+1)
+    setAverage(average-(1/22.5))
+    setPositive(((good)*100)/(all+1))
+  }
+
+
+  return (
+    <div>
+      <button onClick={clicksgood}>Good</button>
+      <button onClick={clicksneutral}>Neutral</button>
+      <button onClick={clicksbad}>Bad</button>
+      <p>Good:{good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad:{bad}</p>
+      <p>All:{all}</p>
+      <p>Average:{average}</p>
+      <p>Positive:{positive}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
